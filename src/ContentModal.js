11fbb21675bc3eb3da88388c52bcca6145854d77
Anna Kravchenko <flamedance.ann@gmail.com>
2022-12-24 17:25:30 +0300

    feat: separate database into the file

--- src/ContentModal.js
+++ src/ContentModal.js
@@ -13,7 +13,7 @@ export default class ContentModal extends Modal {
     template += `
     <div class='modal_article'>
       <div class='modal_article-heading'>
-        <h3 class='modal__heading_title'>${this.title}</h3>
+        <h1 class='modal_heading-title'>${this.title}</h1>
       </div>
       <div class='modal_article-description'>
         <p class='modal_article-description_text'>${this.description}</p>
--- src/index.js
+++ src/index.js
@@ -1,145 +1,18 @@
 import ContentModal from './ContentModal.js';
-
-const data = [
-  {
-    svg: '<svg width="240" height="240" viewBox="-200 -200 400 400"><circle cx="0" cy="40" r="140" fill="#D1495B" /><circle cx="0" cy="-150" r="24" fill="none" stroke="#F79257" stroke-width="4" /><rect x="-35" y="-130" width="70" height="40" fill="#F79257" /></svg>',
-    title: "Christmas bauble",
-    description: "decorates Christmas tree."
-  },
-  {
-    svg: '',
-    title: "Christmas tree",
-    description: "is an evergreen tree decorated with lights and ornaments."
-  },
-  {
-    svg: '',
-    title: "Gingerbread man",
-    description: "is a biscuit cookie made from gingerbread, usually in the shape of a man."
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-  {
-    svg: '',
-    title: "Christmas bauble",
-    description: ""
-  },
-]
+import { data } from './Data.js';
 
 window.onload = function() {
-  const days = Array.from(document.querySelectorAll('.day')),
-        svgs = Array.from(document.querySelectorAll('svg'));
 
-  addTileClickHandler(days, svgs);
+  addTileClickHandler();
 
   setInterval(addClocksHandler, 1000);
 
   addClocksHandler();
 }
 
-const addTileClickHandler = (days) => {
+const addTileClickHandler = () => {
+  const days = Array.from(document.querySelectorAll('.day'));
+
   days.forEach(item => {
     item.addEventListener('click', () => {
       renderModalWindow(data[item.dataset.index]);
