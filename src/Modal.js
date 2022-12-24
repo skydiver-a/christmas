6af631e627bd3eea1793c3a5a28fcb40fda98d64
Anna Kravchenko <flamedance.ann@gmail.com>
2022-12-24 16:41:35 +0300

    feat: render resizing image to modal window

--- src/Modal.js
+++ src/Modal.js
@@ -8,23 +8,42 @@ export default class Modal {
     this.modalCloseBtn = "";
   }
 
-  build(content) {
+  build(content, image) {
     // overlay
     this.overlay = this.createDOMNode(this.overlay, 'div', 'overlay');
     // modal
     this.modal = this.createDOMNode(this.modal, 'div', this.classes);
     // modal content
-    this.modalImage = this.createDOMNode(this.modalImage, 'div', 'modal_img-container');
     this.modalContent = this.createDOMNode(this.modalContent, 'div', 'modal_content');
+    this.modalImage = this.createDOMNode(this.modalImage, 'div', 'modal_img-container');
     // modal close button
     this.modalCloseBtn = this.createDOMNode(this.modalCloseBtn, 'button', 'modal_close-button');
-    this.modalCloseBtn.innerHTML = '<span>&#10005;</span>';
+    this.modalCloseBtn.innerHTML = '<span class="close">&#10005;</span>';
+    // set and append content & image
     this.setContent(content);
-    this.appendModalElements();
-    // bind events
-    this.bindEvents();
+    this.appendModalElements(image);
     // open modal
     this.openModal();
+    // bind events
+    this.bindEvents();
+  }
+
+  bindEvents() {
+    this.modalCloseBtn.addEventListener('click', this.closeModal);
+    this.overlay.addEventListener('click', this.closeModal);
+  }
+
+  openModal() {
+    document.body.append(this.overlay);
+  }
+
+  closeModal(e) {
+    let classes = e.target.classList;
+    if (classes.contains('overlay') || classes.contains('modal_close-button') || classes.contains('close')) {
+      try {
+        document.querySelector('.overlay').remove()
+      } catch (e) {}
+    }
   }
 
   createDOMNode(node, element, ...classes) {
@@ -42,26 +61,11 @@ export default class Modal {
     }
   }
 
-  appendModalElements() {
-    this.modal.append(this.modalImage);
-    this.modal.append(this.modalContent);
+  appendModalElements(image) {
     this.modal.append(this.modalCloseBtn);
+    this.modalImage.innerHTML = image;
+    this.modalContent.append(this.modalImage);
+    this.modal.append(this.modalContent);
     this.overlay.append(this.modal);
-  }
-
-  bindEvents() {
-    this.modalCloseBtn.addEventListener('click', this.closeModal);
-    this.overlay.addEventListener('click', this.closeModal);
-  }
-
-  openModal() {
-    document.body.append(this.overlay);
-  }
-
-  closeModal(e) {
-    let classes = e.target.classList;
-    if (classes.contains('overlay') || classes.contains('modal_close-button')) {
-      document.querySelector('.overlay').remove();
-    }
   }
 }
